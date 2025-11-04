d96198f3a9081801df3c41c97a6665a41da92fb3
Alex Lundberg <lundberg.alex1@gmail.com>
2025-11-04 21:08:27 +0100

    changed the config to use the new azure backend

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 // Toggle between these by commenting/uncommenting
-export const BASE_URL = "https://130.225.170.52:10332";
+export const BASE_URL = "https://jamnaw-backend.salmonpond-0cbcea78.northeurope.azurecontainerapps.io";
 //export const BASE_URL = "http://130.225.170.52:10331";
